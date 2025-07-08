29a4ec11b17dfaf30633935ef827004e8b5b756f
Hugop2san <hugopereira2santos60@gmail.com>
2025-07-08 05:58:33 -0300

    Tratamento de excecoes

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Treino.Entities;
+using System.Globalization;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+
+
+            Console.WriteLine("Enter account data");
+            
+            int number = 2030;
+            Console.Write("Number: "+number);
+            
+            string holder = "Hugo santos";
+            Console.WriteLine("Holder: "+ holder);
+            
+            Console.WriteLine("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+            
+            //double withdrawLimit = 500.00;
+            Console.WriteLine("Withdraw limit: 1k");
+
+            Account account = new Account(number, holder, balance);
+
+
+            Console.WriteLine();
+            Console.WriteLine("account data:");
+            Console.WriteLine($"Number {account.Number}, \nHolder: {account.Holder}, \nBalance: {account.Balance.ToString("F2", new CultureInfo("pt-BR"))}, \nWithdraw Limit {account.WithdrawLimit.ToString("F2", new CultureInfo("pt-BR"))}");
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2", new CultureInfo("pt-BR"))}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Withdraw error: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Input error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+        }
+    }   
+}
